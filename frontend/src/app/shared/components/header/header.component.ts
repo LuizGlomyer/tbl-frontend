3aba4e918cacdf9b50afab4d446202394fde2f02
Luiz Glomyer <glomyerjunior@hotmail.com>
2025-05-31 18:59:13 -0400

    feat: header buttons and drawer

--- frontend/src/app/shared/components/header/header.component.ts
+++ frontend/src/app/shared/components/header/header.component.ts
@@ -1,10 +1,25 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Output,
+} from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [MatIconModule, MatMenuModule, MatButtonModule, RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HeaderComponent { }
+export class HeaderComponent {
+  @Output() openSettings = new EventEmitter<void>();
+
+  onSettingsClick() {
+    this.openSettings.emit();
+  }
+}
--- frontend/src/app/shared/components/page-view/page-view.component.ts
+++ frontend/src/app/shared/components/page-view/page-view.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { FooterComponent } from "../footer/footer.component";
 import { HeaderComponent } from "../header/header.component";
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-page-view',
-  imports: [FooterComponent, HeaderComponent],
+  imports: [FooterComponent, HeaderComponent, MatSidenavModule],
   templateUrl: './page-view.component.html',
   styleUrl: './page-view.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
